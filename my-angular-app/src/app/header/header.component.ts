4bcfaa844b36dde7c2ae516570e829daa9304f0d
michalkrizak <michal.krizak11@seznam.cz>
2025-02-03 08:11:46 +0100

    header

--- my-angular-app/src/app/header/header.component.ts
+++ my-angular-app/src/app/header/header.component.ts
@@ -1,22 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common'; // Import CommonModule
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { LogoutButtonComponent } from '../logout-button/logout-button.component';
 import { SearcBarComponent } from '../searc-bar/searc-bar.component';
 
 
 @Component({
   selector: 'app-header',
-  imports: [LogoutButtonComponent, CommonModule, RouterModule, SearcBarComponent], // Import CommonModule
+  imports: [LogoutButtonComponent, CommonModule, RouterModule, SearcBarComponent, RouterModule], // Import CommonModule
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent implements OnInit {
+
+  constructor(private router: Router) {} 
+
   @Input() user: any = null;
   @Input() products: any = null;
 
   ngOnInit() {
     console.log('User:', this.user);
     console.log('Products:', this.products);
+  }
+
+  ToCart(): void {
+    this.router.navigate(['../cart']);
   }
 }
