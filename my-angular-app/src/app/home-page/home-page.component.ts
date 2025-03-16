76cdca46d14d732c5ef758ad54b8fc45de70150b
michalkrizak <michal.krizak11@seznam.cz>
2025-03-16 11:20:27 +0100

    new product detail

--- my-angular-app/src/app/home-page/home-page.component.ts
+++ my-angular-app/src/app/home-page/home-page.component.ts
@@ -15,16 +15,50 @@ import { SideNavComponent } from '../components/side-nav/side-nav.component';
 import { HeaderComponent } from '../header/header.component';
 import { ProductComponent } from '../products/product/product.component';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { Input, OnInit,  } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { AuthService } from '../services/auth.service';
+import { MatSliderModule } from '@angular/material/slider';
+import { CartService } from '../services/cart-service';
+import { ApiService, IProduct } from '../services/api-service';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home-page',
-  imports: [ProductDetailComponent, HeaderComponent, SearcBarComponent, SideNavComponent, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, CommonModule, FormsModule, SearcBarComponent, LogoutButtonComponent, ProductDetailComponent],
+  imports: [ProductDetailComponent, HeaderComponent, SearcBarComponent, SideNavComponent, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, CommonModule, FormsModule, SearcBarComponent, LogoutButtonComponent, ProductDetailComponent, ProductComponent, MatProgressSpinnerModule, MatSliderModule, MatIcon],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
 export class HomePageComponent {
   @ViewChild(SideNavComponent) sidenavComponent!: SideNavComponent;
-  constructor(private router: Router, private media: MediaMatcher) { }
+  constructor(private router: Router, private media: MediaMatcher,
+    private apiService: ApiService,
+    private authService: AuthService,
+  ) { }
+
+  products: IProduct[] = [];
+  user: any = null;
+
+  ngOnInit(): void {
+    // Použití nového API
+    this.apiService.getAllProducts().subscribe({
+      next: (data) => {
+        this.products = data;
+        console.log(data);
+      },
+      error: (error) => {
+        console.error('Error fetching products:', error);
+      }
+    });
+
+    this.authService.user$.subscribe((user) => {
+      this.user = user;
+      console.log('Current user:', user);
+    });
+  }
+
 
   openSidenav() {
     this.sidenavComponent.toggleSidenav();
--- my-angular-app/src/app/product-detail/product-detail.component.ts
+++ my-angular-app/src/app/product-detail/product-detail.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-product-detail',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.scss'
 })
 export class ProductDetailComponent {
-
+  @Input() product: any;
 }
